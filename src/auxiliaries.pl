fa17515065b5cf2e0feb08841801832466f3e444
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2018-12-09 17:05:32 +0000

    Changed constraint ordering representation to use a simple list. * Ordering of predicates and constants was previously represented as a   series of A > B compounds. However, that complicates testing ordering   later on and is generally a bit cumbersome. I thought about using the   dynamic database to assert and test orders, too, but that way lies   madnesssss. In any case, there's also the detail to consider that the   lexicographic ordering of predicates basically coincides with the   predicate signature, which permits a very simple modification to the   current prove/6 procedure (hopefully coming in a commit near this one).

--- src/auxiliaries.pl
+++ src/auxiliaries.pl
@@ -22,43 +22,80 @@
 %
 %	Assign an automatic ordering to the Herbrand base.
 %
+%	Motivation
+%	----------
+%
+%	The purpose of this predicate is to allow a user to define the
+%	lexicographic and interval ordering of background predicates
+%	declared in an experiment file and their constants, without any
+%	additional work, besides the declaration of those predicates.
+%	Specifically, it is meant to obviate the need for a special
+%	predicate storing a hand-crafted list of each kind of term
+%	in the required order.
+%
+%	The relevant ordering is instead automatically determined a) by
+%	the order in which predicates are declaed as background
+%	knowledge (by including their symbols and aritities in the list
+%	given as the first argument of the background_knowledge/1
+%	predicate), b) in the order in which atoms of background
+%	predicates are generated, or appear in the source file
+%	(depending on whether a predicate is intentionally or
+%	extensionally defined) and, c) the order in which constants
+%	appear in predicates' atoms.
+%
+%	Although this ordering may still require some consideration on
+%	the part of the user, it can be managed as part of the normal
+%	process of declaring background knowledge which the user will
+%	probably have to perform anyway.
+%
+%	Explanation
+%	-----------
+%
 %	Default must be one of [lower, higher], denoting whether
 %	constants found in multiple predicates are ordered according to
 %	their higher, or lower possible ordering.
 %
-%	Predicates is a list of compounds P1 > P2 where P1 and P2 are
-%	symbols of predicates in the background knowledge and P1 is
-%	above P2 in the lexicographic ordering of predicates.
+%	Predicates is a list representing an ordered sequence of unique
+%	predicate symbols {P1, P2,... Pn} where each Pi is the symbol of
+%	a predicate in the background knowledge and Pj is above Pk in
+%	the lexicographic ordering of predicates iff {Pj,Pk} is a
+%	subsequence of the sequence represented by Predicates.
 %
-%	Similarly, Constants is a list of compounds C1 > C2, where each
-%	C1 and C2 are constants in atoms of background predicats and C1
-%	is above C2 in the interval ordering of constants.
+%	Similarly, Constants is a list [C1, C2, ... Cn] where each
+%	Ci is a constant in an atom of a background predicate and Cj
+%	is above Ck in the interval ordering of constants iff [Cj,Ck]
+%	are in Constants.
 %
 %	The two orderings are assigned according to the appearance of
-%	terms in a source file. Specifically:
+%	terms in an experiment file, provided by the user. Specifically:
 %
-%	a) Each predicate is assigned a rank according to is appearance
-%	in the list background_knowledge/1, in an experiment file.
+%	a) Each predicate is assigned a rank equal to its position in
+%	the list in the first argument of background_knowledge/1,
+%	defined in the experiment file.
 %
-%	b) Each constant is assigned one or more indexings I/J/K, where
-%	I is the rank of a predicate that includes it as a term, J is 1
-%	if the constant is the first argument, or 2 if it's the second
-%	argument, of that predicate and K is the index of the atom in
-%	which it appears (which coincides with the index of that clause
-%	if the predicate is defined extensionally).
+%	b) Each constant C is assigned one or more indexing terms I/J/K,
+%	where: I is the rank of a predicate P such that one or more
+%	atoms of P include C as a term; J is 1 if C is the first term or
+%	2 if it's the second term of that predicate; and K is the index
+%	of the atom of P in which C appears as a term (K coincides with
+%	the index, in the dynamic database, of the clause of P
+%	representing that atom, if P is defined extensionally; a clause
+%	index can be obtained in Swi Prolog with the built-in
+%	nth_clause/3).
 %
 %	The relative order of constants then depends on the I/J/K
-%	indexings assigned to them. In an ordered pair C1 > C2, C1 is
-%	above C2 in the total interval ordering iff I1/J1/K1 > I2/J2/K2
-%	(where the indexing subscripts match the contstant subscripts).
+%	indexing terms assigned to them. Given a pair of constants, C1,
+%	C2, C1 is above C2 in the total interval ordering iff I1/J1/K1 >
+%	I2/J2/K2 (where the indexing subscripts match the contstant
+%	subscripts).
 %
 %	When a constant appears multiple times in different atoms of
 %	possibly different predicates, it is assigned multiple
 %	indexings. This is resolved by taking into account the
 %	value of Default. If Default is lower, the indexing resulting in
-%	the lowest possible ordering is assinged to the constant. If
-%	Default is higher, the highest ordering is assigned to it
-%	instead.
+%	the lowest possible ordering is used to determine the order of
+%	the constant in Constants. If Default is higher, the highest
+%	ordering is used instead.
 %
 order_constraints(Ps,Cs,D):-
 	must_be(oneof([lower,higher]),D)
@@ -68,23 +105,22 @@ order_constraints(Ps,Cs,D):-
 	,constants_indexing(M,BK,Is)
 	,unique_indices(Is,Is_,D)
 	,sort(1,@<,Is_,Is_s)
-	,indexing_constraints(Is_s, Cs).
+	,findall(C
+		,member(c(_/_/_,C),Is_s)
+		,Cs).
 
 
 %!	predicate_order(+Background,-Order) is det.
 %
 %	Order predicates by their appearance in the Background.
 %
-predicate_order(BK,Os):-
+%	@tbd Note that this is essentially the same as
+%	predicate_signature/1.
+%
+predicate_order(BK,Ps):-
 	findall(F
 	       ,member(F/_,BK)
-	       ,Ps)
-	,predicate_order(Ps,[],Os).
-
-predicate_order([_],Os,Os):-
-	!.
-predicate_order([P1,P2|Ps],Acc,Bind):-
-	predicate_order([P2|Ps],[P1 > P2|Acc],Bind).
+	       ,Ps).
 
 
 %!	constants_indexing(+Module,+Background,-Indexed) is det.
@@ -128,26 +164,6 @@ unique_indices(higher,[c(I/J/K,C1),c(_/_/_,C1)|Ss],Acc,Bind):-
 	,unique_indices(higher,[c(I/J/K,C1)|Ss],Acc,Bind).
 unique_indices(O,[c(I/J/K,C)|Ss],Acc,Bind):-
 	unique_indices(O,Ss,[c(I/J/K,C)|Acc],Bind).
-
-
-%!	indexing_constraints(+Indexing,-Constraints) is det.
-%
-%	Generate a set of Constraints from an Indexing order.
-%
-indexing_constraints(Is,Cs):-
-	indexing_constraints(Is,[],Cs).
-
-
-%!	indexing_constraints(+Indexing,+Acc,-Constraints) is det.
-%
-%	Business end of indexing_constraints/2.
-%
-indexing_constraints([_],Cs,Cs_):-
-	reverse(Cs, Cs_)
-	,!.
-indexing_constraints([c(_/_/_,C1),c(I/J/K,C2)|Is],Acc,Bind):-
-	indexing_constraints([c(I/J/K,C2)|Is],[C1 > C2|Acc],Bind).
-
 
 
 
