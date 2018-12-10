f8494db1308d5912f837a213c2b92f0b1aeefc53
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2018-12-10 00:41:21 +0000

    Added metarules and background to learn father/2, male/2. * male/2 currently goes infinite, even though we _can_ totally invent it   as part of learning father/2.

--- data/tiny_kinship.pl
+++ data/tiny_kinship.pl
@@ -5,11 +5,23 @@
 		       ,father/2
 		       ,mother/2
 		       ,parent/2
+		       ,male/1
+		       ,female/1
 		       ]).
 
+% Learn father/2
+%background_knowledge([parent/2,male/1]).
+% Learn grandfather/2
 background_knowledge([father/2,mother/2]).
+% Learn male/1
+%background_knowledge([male/1,female/1]).
 
+% Learn father/2
+%metarules([chain,projection]).
+% Learn grandfather/2
 metarules([chain,inverse]).
+% Learn male/1
+%metarules([identity,projection]).
 
 father(stathis, kostas).
 father(stefanos, dora).
@@ -24,11 +36,28 @@ parent(X, Y):-
 parent(X, Y):-
 	mother(X,Y).
 
+male(stathis).
+male(stefanos).
+male(kostas).
+
+female(dora).
+female(stassa).
+female(alexandra).
+female(paraskevi).
+
+positive_example(male,male(A,A)):-
+	male(A).
+positive_example(father,father(A,B)):-
+	father(A,B).
 positive_example(grandfather,grandfather(A,B)):-
 	grandfather(A,B).
 
 negative_example(grandfather,grandfather(A,B)):-
 	grandmother(A,B).
+negative_example(father,father(A,B)):-
+	mother(A,B).
+negative_example(male,male(A,A)):-
+	female(A).
 
 % Target theory.
 grandfather(A,B):- father(A,C), parent(C,B).
