e8691a7dbf82bf80c4f5a4db6e69a6cb7a823d61
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2019-06-01 22:29:14 +0100

    Improved even/1 example. * This now stops at 0, which actually makes sense because the background   predicate used is predecessor/2, the opposite of successor/2. Use of   successor/2 forces the theory learned in the previous version of this   experiment file, where the recursion terminates at 101. The example in   the MIL paper on predicate invention targets a theory where successor   is inverted to invent predecessor- but this never happens in the   theories learned by Thelma, apparently because it's easier to use   successor/2 directly. I'm not sure I 100% grok what's going on here   though. The programs learned with predecessor/2 and successor/2 are   the same, I think.

--- data/even.pl
+++ data/even.pl
@@ -2,8 +2,65 @@
 	       ,metarules/2
 	       ,positive_example/2
 	       ,negative_example/2
-	       ,successor/2
+	       ,predecessor/2
 	       ]).
+
+/** <module> Experiment file for even/1 generator and acceptor.
+
+Learns a generator and acceptor of even integers from 0 to 100 by
+inventing a generator and acceptor for odd integers from 0 to 100.
+
+even/1 and the invented predicate corresponding to odd/1, even_1/1 are
+definted in terms of each other making this an example of using MIL to
+learn mutually recursive theories.
+
+Usage
+=====
+
+1. Set depth_limits(3,1).
+
+2. Run the query:
+
+?- experiment_data(even/1,_Pos,_Neg,_BK,_MS), learn(_Pos,_Neg,_Prog), print_clauses(_Prog).
+% Clauses: 1; Invented: 0
+% Clauses: 2; Invented: 0
+% Clauses: 2; Invented: 1
+% Clauses: 3; Invented: 0
+% Clauses: 3; Invented: 1
+even(0).
+even(A):-predecessor(A,B),even_1(B).
+even_1(A):-predecessor(A,B),even(B). % Invention of odd/1
+true .
+
+3. Consult hypothesis and test:
+
+?- findall(X, (even:even(X), \+ (X == 0 ; 0 is X mod 2)), Xs).
+Xs = [].
+
+?- findall(X, (even:even(X), (X == 0 ; 0 is X mod 2)), _Xs), writeln(_Xs).
+[0,0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100]
+
+Expanding the range of the learned theory
+=========================================
+
+The learned definition of even/1 only generates and accepts integers
+from 0 to 100. This is the effect of the line between(0,100,A) in the
+body of predecessor/2. The reason for this restriction is that a)
+allowing an infinite range means learning must take an infinite time
+and, b) a general theory can be learned from a small range, even one
+smaller than [0,100].
+
+If required, the range covered by even/1 can be extended (or reduced)
+either during learning, or afterwards, by changing the 100 in
+between(0,100,A) with a different value.
+
+For instance, by changing between(0,100) to between(0,inf) the learned
+theory can cover the full range of integers from to to infinity.
+
+But if you do that during learning you'll wait a long time to see the
+nice results.
+
+*/
 
 configuration:metarule(unit_monadic,[P,X],[X],mec(P,X):-true).
 configuration:metarule(postcon_unit,[P,Q,R],[X,Y],(mec(P,X):-mec(Q,X,Y),mec(R,Y))).
@@ -11,7 +68,7 @@ configuration:metarule(postcon_unit,[P,Q,R],[X,Y],(mec(P,X):-mec(Q,X,Y),mec(R,Y)
 configuration:order_constraints(unit_monadic,_Ss,_Fs,[],[]).
 configuration:order_constraints(postcon_unit,_Ss,[X,Y],[],[X>Y]).
 
-background_knowledge(even/1,[successor/2]).
+background_knowledge(even/1,[predecessor/2]).
 
 metarules(even/1,[unit_monadic,postcon_unit,inverse]).
 
@@ -21,37 +78,13 @@ positive_example(even/1,even(A)):-
 negative_example(even/1,even(A)):-
 	member(A,[1,3,5,7,9,11]).
 
-successor(A,B):-
+%!	predecesor(?A,?B) is nondet.
+%
+%	True when A > B and both are integers.
+%
+%	@tbd Use of predecessor taken from Metagol examples by Andrew
+%	Cropper (in metagol/examples/mutual_recursion.pl).
+%
+predecessor(A,B):-
 	between(0,100,A)
-	,succ(A,B).
-
-/* Target theory:
-even(0).
-even(A):- even_1(A,B), even_2(B).
-even_1(A,B):- succ(B,A).
-even_2(A):- even_1(A,B), even(B).
-*/
-
-
-/* These are actually ... odd.
-even_1(101).
-even(A):-successor(A,B),even_1(B).
-even_1(A):-successor(A,B),even(B).
-
-even(0).
-even_1(101).
-even(A):-successor(A,B),even_1(B).
-even_1(A):-successor(A,B),even(B).
-
-These were learned with identity rather than inverse.
-Why? I got negative examples that should have stopped them.
-
-Ooops. Haha. Actually, even_1/1 is odd. even/1 is even!
-But- it stops at 101. Meh.
-
-Can be tested with:
-
-?- findall(X, (even:even(X), \+ (X == 0 ; 0 is X mod 2)), Xs).
-Xs = [].
-
-*/
+	,succ(B,A).
